fda9bf910a7b31e302191c926ae6c62c4c1ae4aa
Johannes Sappl <johannes.sappl@chello.at>
2024-02-28 14:43:17 +0100

    feat: init `torch.hub` entrypoint #3

---
+++ hubconf.py
@@ -0,0 +1,22 @@
+"""Entrypoints for generic PyTorch models."""
+
+from typing import TYPE_CHECKING
+
+import torchmodels
+
+if TYPE_CHECKING:
+    import torch.nn as nn
+
+dependencies = ["torch"]
+
+
+def multilayer_perceptron(neurons: list[int]) -> "nn.Module":
+    """Load the multilayer perceptron model architecure.
+
+    Args:
+        neurons: Specify the number of neurons for each layer.
+
+    Returns:
+        An instance of the `torchmodels.MultilayerPerceptron` class.
+    """
+    return torchmodels.MultilayerPerceptron(neurons)
